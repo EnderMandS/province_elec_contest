e97996404ca0d7019c1edbe32b36d1de0a06ea1c
M <952025764@qq.com>
2022-07-20 14:26:17 +0800

    结果滤波、封装class

--- cam/main.py
+++ cam/main.py
@@ -3,39 +3,146 @@
 import sensor, image, time
 from pyb import LED, Timer, Pin
 
+
 # Const define
 RED_ = 0
 GREEN_ = 1
+YELLOW_ = 2
+
 PI_ = 3.14159265358979
-R_ = 25
+
+ROUNDNESS_THRESHOLD_ = 0.85
+FILTER_FRAME_CNT_ = 5
+
+GREEN_THRESHOLD_ = (72, 95, -128, -48, 12, 86)
+RED_THRESHOLD_ = (50, 80, 50, 100, 30, 100)
+YELLOW_THRESHOLD_ = (70, 100, -20, 20, 0, 127)
+
+YELLOW_X_TOLERANCE_ = 10
+YELLOW_Y_TOLERANCE_ = 10
 
 # Board LED init
 red_led     = LED(1)
 green_led   = LED(2)
 blue_led    = LED(3)
-ir_leds     = LED(4)
 
-# Traffic light threadhold
-GREEN_THRESHOLD_ = (50, 100, -128, 0, 0, 127)
-RED_THRESHOLD_ = (0, 100, 0, 127, 20, 127)
-YELLOW_THRESHOLD_ = (70, 100, -20, 127, -20, 127)
-YELLOW_X_TOLERANCE_ = 10
-YELLOW_Y_TOLERANCE_ = 10
+class TrafficLightFilter:
+    def __init__(self):
+        self.count = 0
+        self.status = False
 
-# I/O init
-valid_pin = Pin('P5', Pin.OUT_PP, Pin.PULL_DOWN)    # 数据输出有效IO
-signal_pin = Pin('P6', Pin.OUT_PP, Pin.PULL_DOWN)   # 数据输出IO
-valid_pin.low()     # 定义高电平为数据有效
-signal_pin.low()    # 定义高电平为绿灯、低电平为红灯
+    def iterate(self, status_input):
+        if status_input == self.status :
+            self.count = 0
+        else :
+            self.count = self.count + 1
+            if self.count >= FILTER_FRAME_CNT_ :
+                self.reverse()
 
-# Timer callback
-def Tim14Callback(timer):   # LED工作指示
-    pass
-    #green_led.toggle()
+    def clear(self) :
+        self.count = 0
+        self.status = False
 
-# Timer init
-tim14 = Timer(14, freq = 2)
-tim14.callback(Tim14Callback)
+    def reverse(self) :
+        self.count = 0
+        if self.status :
+            self.status = False
+        else :
+            self.status = True
+
+
+class TrafficLight:
+    def __init__(self):
+        self.current_color = GREEN_
+        self.available = False
+        # Filter init
+        self.red_filter     = TrafficLightFilter()
+        self.yellow_filter  = TrafficLightFilter()
+        self.green_filter   = TrafficLightFilter()
+        self.total_filter   = TrafficLightFilter()
+
+    def trafficColorGet(self, img, c, color_threshold):
+        rec_area = ( c.x()-c.r(), c.y()-c.r(), 2*c.r(), 2*c.r() )
+        x_stride = y_stride = c.r()-10
+        area_threshold = (int)(PI_*c.r()*c.r()*0.25)
+
+        if x_stride<5:
+            x_stride = y_stride = 5
+        if area_threshold < 5:
+            area_threshold = 5
+
+        return img.find_blobs([color_threshold],
+            roi = rec_area,
+            x_stride = x_stride, y_stride = y_stride,
+            area_threshold = area_threshold,
+            pixels_threshold = 10, merge = False)
+
+    def trafficLightFind(self, img):
+        circles = img.find_circles(threshold = 5000,
+            x_margin = 10, y_margin = 10, r_margin = 10,
+            r_min = 10, r_max = 50, r_step = 2)
+
+        if circles == [] :
+            self.total_filter.iterate(False)
+        else :
+            red_find = yellow_find = green_find = False
+
+            for c in circles:
+                rec_area = ( c.x()-c.r(), c.y()-c.r(), 2*c.r(), 2*c.r() )
+                img.draw_rectangle(rec_area, color = (0, 0, 255))
+
+                red_blobs = self.trafficColorGet(img, c, RED_THRESHOLD_)
+                for blo in red_blobs:
+                    if blo.roundness()>ROUNDNESS_THRESHOLD_ :
+                        img.draw_rectangle(blo.rect(), color = (255, 0, 0))
+                        red_find = True
+
+                green_blobs = self.trafficColorGet(img, c, GREEN_THRESHOLD_)
+                for blo in green_blobs:
+                    if blo.roundness()>ROUNDNESS_THRESHOLD_ :
+                        img.draw_rectangle(blo.rect(), color = (0, 255, 0))
+                        green_find = True
+
+                yellow_blobs = self.trafficColorGet(img, c, YELLOW_THRESHOLD_)
+                for blo in yellow_blobs:
+                    if blo.roundness()>ROUNDNESS_THRESHOLD_ :
+                        img.draw_rectangle(blo.rect(), color = (255, 255, 0))
+                        yellow_find = True
+
+            if (not red_find) and (not green_find) and (not yellow_find) :
+                self.total_filter.iterate(False)
+            else :
+                self.total_filter.iterate(True)
+
+            self.red_filter.iterate(red_find)
+            self.yellow_filter.iterate(yellow_find)
+            self.green_filter.iterate(green_find)
+
+    def filterOutcome(self):
+        if self.total_filter.status :
+            self.available = True
+            if self.red_filter.status :
+                self.current_color = RED_
+                red_led.on()
+            elif self.yellow_filter.status :
+                self.current_color = YELLOW_
+                red_led.on()
+                green_led.on()
+            elif self.green_filter.status :
+                self.current_color = GREEN_
+                green_led.on()
+            else :
+                self.available = False
+                red_led.off()
+                green_led.off()
+                blue_led.off()
+
+        else :
+            self.available = False
+            red_led.off()
+            green_led.off()
+            blue_led.off()
+
 
 # Sensor init
 sensor.reset()
@@ -43,60 +150,24 @@ sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QQVGA)
 sensor.skip_frames(time = 2000)
 sensor.set_auto_gain(True)
-sensor.set_auto_whitebal(True)
+sensor.set_auto_whitebal(False)
 
 # Clock init
 clock = time.clock()
 
-def trafficColorGet(img, c, color_threshold):
-    rec_area = ( c.x()-c.r(), c.y()-c.r(), 2*c.r(), 2*c.r() )
-    x_stride = y_stride = c.r()-10
-    area_threshold = (int)(PI_*c.r()*c.r()*0.25)
+## Timer callback
+#def Tim14Callback(timer):   # LED工作指示
+    #pass
 
-    if x_stride<5:
-        x_stride = y_stride = 5
-    if area_threshold < 5:
-        area_threshold = 5
+## Timer init
+#tim14 = Timer(14, freq = 1)
+#tim14.callback(Tim14Callback)
 
-    return img.find_blobs([color_threshold],
-        roi = rec_area,
-        x_stride = x_stride, y_stride = y_stride,
-        area_threshold = area_threshold,
-        pixels_threshold = 10, merge = False)
-
-
-def trafficLightFind(img):
-    circles = img.find_circles(threshold = 5250,
-        x_margin = 10, y_margin = 10, r_margin = 10,
-        r_min = 10, r_max = 40, r_step = 5)
-
-    for c in circles:
-        rec_area = ( c.x()-c.r(), c.y()-c.r(), 2*c.r(), 2*c.r() )
-        #img.draw_rectangle(rec_area)
-
-        red_blobs   = trafficColorGet(img, c, RED_THRESHOLD_)
-        green_blobs = trafficColorGet(img, c, GREEN_THRESHOLD_)
-
-
-        if (not(red_blobs)) and (not(green_blobs)) :
-            continue
-        if (not(red_blobs)) and green_blobs :
-            for blo in green_blobs:
-                img.draw_rectangle(blo.rect(), color = (0, 255, 0))
-        elif red_blobs and (not(green_blobs)) :
-            for blo in red_blobs:
-                img.draw_rectangle(blo.rect(), color = (255, 0, 0))
-        else:
-            for red_b in red_blobs:
-                for green_b in green_blobs:
-                    dx = red_b.cxf()-green_b.cxf()
-                    dy = red_b.cyf()-green_b.cyf()
-                    if dx<YELLOW_X_TOLERANCE_ and dy<YELLOW_Y_TOLERANCE_ :
-                        img.draw_rectangle(green_b.rect(),color = (255, 255, 0))
-
+traffic_light = TrafficLight()
 
 while(True):
     clock.tick()
     img = sensor.snapshot()
-    trafficLightFind(img)
+    traffic_light.trafficLightFind(img)
+    traffic_light.filterOutcome()
     print(clock.fps())
