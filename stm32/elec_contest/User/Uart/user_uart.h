3e55673d91cdade8cd747e82dc93183a3eb915eb
M <952025764@qq.com>
2022-07-24 14:03:52 +0800

    添加c++用户串口DMA接收框架

---
+++ stm32/elec_contest/User/Uart/user_uart.h
@@ -0,0 +1,30 @@
+//
+// Created by M on 2022/07/24.
+//
+
+#ifndef ELEC_CONTEST_USER_UART_H
+#define ELEC_CONTEST_USER_UART_H
+
+#include "main.h"
+#include "FreeRTOS.h"
+#include "cmsis_os2.h"
+#include "string"
+
+namespace user_uart {
+  class UartReceive {
+  public:
+    UartReceive(UART_HandleTypeDef *huart, uint32_t data_length, const std::string &queue_name);
+    HAL_StatusTypeDef uartReceiveStart();
+    HAL_StatusTypeDef uartReceiveStop();
+    void uartCallback();
+
+    osMessageQueueId_t queue_handle;
+    uint32_t length;
+
+  private:
+    uint8_t *buffer;
+    UART_HandleTypeDef *huart;
+  };
+}
+
+#endif //ELEC_CONTEST_USER_UART_H
